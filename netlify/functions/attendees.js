cd87ac33a4000166a315d56fec033e78c89527ea
Martin Martinez <martinjmartinez@users.noreply.github.com>
2024-09-23 12:50:43 -0400

    Update attendees.js

--- netlify/functions/attendees.js
+++ netlify/functions/attendees.js
@@ -73,5 +73,130 @@ app.get('/.netlify/functions/attendees', async (req, res) => {
   }
 });
 
+app.get('/.netlify/functions/attendees/add_company', async (req, res) => {
+    const { event_id, first, last, company, attendee_role_id, is_entity, attendee_type, virtual } = req.query; // Destructure query params
+  
+    if (!event_id) {
+      return res.status(400).send({ error: 'Missing event_id parameter' });
+    }
+  
+    const oauth = OAuth({
+      consumer: {
+        key: 'igapi',  // Your consumer key
+        secret: 'MeetMax12!',  // Your consumer secret
+      },
+      signature_method: 'HMAC-SHA1',
+      hash_function(baseString, key) {
+        return crypto.createHmac('sha1', key).update(baseString).digest('base64');
+      },
+    });
+  
+    // Construct the URL with dynamic parameters
+    let url =`https://www.meetmax.com/sched/service/attendee/add?event_id=${event_id}&first=${first}&last=${last}&company=${company}&attendee_role_id=${attendee_role_id}&is_entity="Y"&attendee_type="E"&virtual="Y"`
+      // Add optional parameters if they are present
+      if (search_fld) {
+          url += `&search_fld=${search_fld}`;
+        }
+      if (search_for) {
+          url += `&search_for=${search_for}`;
+      }
+    const request_data = {
+      url: url,
+      method: 'GET',
+    };
+  
+    try {
+      console.log('Making request to:', request_data.url);
+  
+      const response = await fetch(request_data.url, {
+        method: request_data.method,
+        headers: {
+          ...oauth.toHeader(oauth.authorize(request_data)),
+        },
+      });
+  
+      if (!response.ok) {
+        throw new Error(`Error: ${response.statusText}`);
+      }
+  
+      let responseData;
+      if (data_type === 'JSON') {
+        responseData = await response.json();
+        console.log('JSON data retrieved:', responseData);
+      } else if (data_type === 'XML') {
+        responseData = await response.text(); // Assume XML by default
+        console.log('XML data retrieved:', responseData);
+      }
+  
+      // Send the response back to the client
+      res.status(200).send(responseData);
+    } catch (error) {
+      console.error('Error occurred:', error.message);
+      res.status(500).send(JSON.stringify({ error: error.message }));
+    }
+  });
+
+  app.get('/.netlify/functions/attendees/add_attendee', async (req, res) => {
+    const { event_id, first, last, company, attendee_role_id, is_entity, attendee_type, virtual } = req.query; // Destructure query params
+  
+    if (!event_id) {
+      return res.status(400).send({ error: 'Missing event_id parameter' });
+    }
+  
+    const oauth = OAuth({
+      consumer: {
+        key: 'igapi',  // Your consumer key
+        secret: 'MeetMax12!',  // Your consumer secret
+      },
+      signature_method: 'HMAC-SHA1',
+      hash_function(baseString, key) {
+        return crypto.createHmac('sha1', key).update(baseString).digest('base64');
+      },
+    });
+  
+    // Construct the URL with dynamic parameters
+    let url =`https://www.meetmax.com/sched/service/attendee/add?event_id=${event_id}&first=${first}&last=${last}&company=${company}&attendee_role_id=${attendee_role_id}`
+      // Add optional parameters if they are present
+      if (search_fld) {
+          url += `&search_fld=${search_fld}`;
+        }
+      if (search_for) {
+          url += `&search_for=${search_for}`;
+      }
+    const request_data = {
+      url: url,
+      method: 'GET',
+    };
+  
+    try {
+      console.log('Making request to:', request_data.url);
+  
+      const response = await fetch(request_data.url, {
+        method: request_data.method,
+        headers: {
+          ...oauth.toHeader(oauth.authorize(request_data)),
+        },
+      });
+  
+      if (!response.ok) {
+        throw new Error(`Error: ${response.statusText}`);
+      }
+  
+      let responseData;
+      if (data_type === 'JSON') {
+        responseData = await response.json();
+        console.log('JSON data retrieved:', responseData);
+      } else if (data_type === 'XML') {
+        responseData = await response.text(); // Assume XML by default
+        console.log('XML data retrieved:', responseData);
+      }
+  
+      // Send the response back to the client
+      res.status(200).send(responseData);
+    } catch (error) {
+      console.error('Error occurred:', error.message);
+      res.status(500).send(JSON.stringify({ error: error.message }));
+    }
+  });
 // Correctly export the handler
 module.exports.handler = serverless(app);
